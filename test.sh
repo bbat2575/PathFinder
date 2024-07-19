a0bd20dcbc6f066b4518cf3838195360bd488593
Bassam Batch <bbat2575@uni.sydney.edu.au>
2024-07-19 13:46:03 +1000

    Path finder repo

--- test.sh
+++ test.sh
@@ -11,22 +11,19 @@ for input_file in $test_files; do
     # Print test file name
     echo "Testing $input_file"
 
-    # Generate output file name
-    output_file="${input_file%.in}.out"
+    # Generate expected output file name
+    expected_file="${input_file%.in}.expected"
     
     # Run the program with input from the current test file
-    output=$(cat "$input_file" < $program)
+    output=$($program < "$input_file")
     
-    # Read the expected output from the corresponding .out file
-    expected_output=$(cat "$output_file")
+    # Read the expected output from the corresponding .expected file
+    expected_output=$(cat "$expected_file")
     
     # Compare the program's output with the expected output
     if [ "$output" != "$expected_output" ]; then
-        echo "Test failed for input file: $input_file"
-        echo "Expected output:"
-        echo "$expected_output"
-        echo "Actual output:"
-        echo "$output"
+        echo "Test failed..."
+        diff <(echo "$expected_output") <(echo "$output")
     else
         echo "Test passed!"
     fi
